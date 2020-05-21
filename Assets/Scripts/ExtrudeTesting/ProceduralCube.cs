ac96c3d89daab23a7975ed97edf89983eadd3bed
alecphobia95 <alecphobia95@gmail.com>
2020-05-21 18:43:31 -0400

    Set up
    Need to finish up later, got basic scripts written

---
+++ Assets/Scripts/ExtrudeTesting/ProceduralCube.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class ProceduralCube : MonoBehaviour
+{
+    Mesh mesh;
+    List<Vector3> vertices;
+    List<int> triangles;
+
+    private void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    private void Start()
+    {
+        MakeCube();
+        UpdateMesh();
+    }
+
+    private void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+    }
+
+    private void MakeCube()
+    {
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+
+        for (int x = 0; x < 6; x++)
+        {
+            MakeFace(x);
+        }
+    }
+
+    private void MakeFace(int dir)
+    {
+        vertices.AddRange(CubeMeshData.faceVertices(dir));
+        int vCount = vertices.Count;
+
+        triangles.Add(vCount - 4);
+        triangles.Add(vCount - 4 + 1);
+        triangles.Add(vCount - 4 + 2);
+        triangles.Add(vCount - 4);
+        triangles.Add(vCount - 4 + 2);
+        triangles.Add(vCount - 4 + 3);
+    }
+
+}
