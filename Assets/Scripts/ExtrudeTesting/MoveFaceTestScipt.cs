db1359e90cf32d3c47509c7f2104ab9e0024b69b
alecphobia95 <alecphobia95@gmail.com>
2020-05-27 18:20:18 -0400

    Face Moving
    Got a face moving script working, now need to clean up and make sure face does not seperate

---
+++ Assets/Scripts/ExtrudeTesting/MoveFaceTestScipt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFaceTestScipt : MonoBehaviour
+{
+
+    public ProceduralCube cube;
+    public float moveSpeed = 1;
+    public float pos = 1;
+
+    private void Update()
+    {
+        if(pos < 5)
+        {
+            pos += moveSpeed * Time.deltaTime;
+            cube.MoveFace("z+", pos);
+        }
+    }
+
+}
--- Assets/Scripts/ExtrudeTesting/ProceduralCube.cs
+++ Assets/Scripts/ExtrudeTesting/ProceduralCube.cs
@@ -53,4 +53,99 @@ public class ProceduralCube : MonoBehaviour
         triangles.Add(vCount - 4 + 3);
     }
 
+    //Trying to clean up these switches
+    public void MoveFace(string faceDir, float newPos)
+    {
+        Vector3 currentVPos;
+        int[] arrayPos = new int[4];
+        switch (faceDir)
+        {
+            case "x+":
+                arrayPos[0] = 0;
+                arrayPos[1] = 3;
+                arrayPos[2] = 5;
+                arrayPos[3] = 6;
+
+                for (int i = 0; i < arrayPos.Length; i++)
+                {
+                    currentVPos = vertices[arrayPos[i]];
+                    currentVPos.x = newPos;
+                    vertices[arrayPos[i]] = currentVPos;
+                }
+
+                break;
+            case "x-":
+                arrayPos[0] = 1;
+                arrayPos[1] = 2;
+                arrayPos[2] = 4;
+                arrayPos[3] = 7;
+
+                for (int i = 0; i < arrayPos.Length; i++)
+                {
+                    currentVPos = vertices[arrayPos[i]];
+                    currentVPos.x = newPos;
+                    vertices[arrayPos[i]] = currentVPos;
+                }
+                break;
+            case "z+":
+                arrayPos[0] = 0;
+                arrayPos[1] = 1;
+                arrayPos[2] = 2;
+                arrayPos[3] = 3;
+
+                for (int i = 0; i < arrayPos.Length; i++)
+                {
+                    currentVPos = vertices[arrayPos[i]];
+                    currentVPos.z = newPos;
+                    vertices[arrayPos[i]] = currentVPos;
+                }
+                break;
+            case "z-":
+                arrayPos[0] = 4;
+                arrayPos[1] = 5;
+                arrayPos[2] = 6;
+                arrayPos[3] = 7;
+
+                for (int i = 0; i < arrayPos.Length; i++)
+                {
+                    currentVPos = vertices[arrayPos[i]];
+                    currentVPos.z = newPos;
+                    vertices[arrayPos[i]] = currentVPos;
+                }
+                break;
+            case "y+":
+                arrayPos[0] = 0;
+                arrayPos[1] = 1;
+                arrayPos[2] = 4;
+                arrayPos[3] = 5;
+
+                for (int i = 0; i < arrayPos.Length; i++)
+                {
+                    currentVPos = vertices[arrayPos[i]];
+                    currentVPos.y = newPos;
+                    vertices[arrayPos[i]] = currentVPos;
+                }
+                break;
+            case "y-":
+                arrayPos[0] = 2;
+                arrayPos[1] = 3;
+                arrayPos[2] = 6;
+                arrayPos[3] = 7;
+
+                for (int i = 0; i < arrayPos.Length; i++)
+                {
+                    currentVPos = vertices[arrayPos[i]];
+                    currentVPos.y = newPos;
+                    vertices[arrayPos[i]] = currentVPos;
+                }
+                break;
+            default :
+                Debug.Log("Invalid faceDir string");
+                break;
+        }
+
+        UpdateMesh();
+
+    }
+
 }
