9aa9d4f10a6cdab55bb231f0eb2972e52be6e7bd
nas <nmuhammad121@gmail.com>
2020-05-31 16:26:53 -0400

    Add basic main menu

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+    public GameObject InstructionsPanel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("PrototypingScene");
+    }
+
+    public void ShowInstructionsPanel()
+    {
+        InstructionsPanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMain()
+    {
+        InstructionsPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
