5f8013c69f8d4f7d1268609a3cf7f0280f963d33
nas <nmuhammad121@gmail.com>
2020-06-10 21:47:35 -0400

    -Code cleanup -Rename BaseAbility to FaceAbility -Add extra properties to FaceAbility -Convert prototype elevator code to ability

--- Assets/Scripts/Abilities/FaceAbility.cs
+++ Assets/Scripts/Abilities/FaceAbility.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IBaseAbility
+public interface IFaceAbility
 {
     Face AbilityFace { get; set; }
     bool IsInitialized { get; set; }
     bool IsActing { get; set; }
+    int AbilityTimes { get; set; }
 
     void InitializeAbility(Face face);
 }
--- Assets/Scripts/Face.cs
+++ Assets/Scripts/Face.cs
@@ -9,8 +9,7 @@ public class Face : MonoBehaviour
 {
     private NavMeshPath path;
     RaycastHit m_HitInfo = new RaycastHit();
-
-    private bool _activatedAbility;
+    
     private Vector3 _target;
     public Renderer _rend;
     private bool _selected;
@@ -18,9 +17,8 @@ public class Face : MonoBehaviour
     private static Material _defaultMat;
     private static Material _selectedMat;
     private static Material _abilityMat;
-
-    public bool hasAbility;
-    public IBaseAbility Ability;
+    
+    public IFaceAbility Ability;
     public Transform Parent;
     public Vector3[] Vertices;
     public int FaceId;
@@ -30,8 +28,6 @@ public class Face : MonoBehaviour
     {
         path = new NavMeshPath();
         _rend = GetComponent<Renderer>();
-        _activatedAbility = false;
-        hasAbility = false;
         Parent = gameObject.transform.parent;
         
         _defaultMat = new Material(Shader.Find("Unlit/ColorZAlways"));
@@ -67,40 +63,84 @@ public class Face : MonoBehaviour
 
             if (Vector3.Distance(facePoint, agentPoint) <= 1.0f && Input.GetKeyDown(KeyCode.E))
             {
-                Debug.Log("hit");
-                Debug.Log(facePoint);
-                Debug.Log(agentPoint);
-                // _activatedAbility = true;
-                // Vector3 target = new Vector3(facePoint.x, facePoint.y + 3, facePoint.z);
-                // _target = target;
-                // GameManager.SharedInstance.playerAgent.transform.parent = transform.parent;
-                // GameManager.SharedInstance.playerAgent.agent.enabled = false;
+                if (Ability.AbilityTimes <= 0)
+                    return;
                 Ability.InitializeAbility(this);
             }
-
-            // if (!_activatedAbility)
-            //     return;
-            // float step = 3 * Time.deltaTime;
-            // gameObject.transform.parent.position =
-            //     Vector3.MoveTowards(gameObject.transform.parent.position, _target, step);
-            // GameManager.SharedInstance.playerAgent.agent.nextPosition = gameObject.transform.parent.position;
-            // if (Vector3.Distance(gameObject.transform.parent.position, _target) < .001f)
-            // {
-            //     _activatedAbility = false;
-            //     var meshSurface = GameManager.SharedInstance.edgeManager._meshSurface;
-            //     meshSurface.UpdateNavMesh(meshSurface.navMeshData);
-            //     StartCoroutine(SetAgentPosition());
-            // }
         }
         else
         {
             if (_selectedFace == this && Input.GetKeyDown(KeyCode.E))
             {
-                // hasAbility = true;
-                _selectedFace._rend.material = _abilityMat;
+                _rend.material = _abilityMat;
                 _selectedFace = null;
-                Ability = gameObject.AddComponent<ColorChangeAbility>();
+                Ability = gameObject.AddComponent<ElevatorAbility>();
             }
+        }
+    }
+
+    private void PlayModeMouseDown()
+    {
+        var ray = GameManager.SharedInstance.MainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
+            GameManager.SharedInstance.playerAgent.agent.destination = m_HitInfo.point;
+        if (gameObject.CompareTag("Finish"))
+            GameManager.SharedInstance.playerAgent.goalPoint = m_HitInfo.point;
+
+        if (NavMesh.CalculatePath(GameManager.SharedInstance.playerAgent.transform.position,
+            m_HitInfo.point,
+            NavMesh.AllAreas,
+            path))
+            Debug.Log("PATH!");
+
+        if (path.corners.Length < 2)
+            return;
+
+        List<Vector3> moverPoints = new List<Vector3>();
+        for (int i = 1; i < path.corners.Length; i++)
+        {
+            Vector3 moverPoint = new Vector3(path.corners[i].x,
+                GameManager.SharedInstance.playerAgent.transform.position.y,
+                path.corners[i].z);
+            moverPoints.Add(moverPoint);
+        }
+
+        // GameManager.SharedInstance.playerAgent.mover.setPositions = moverPoints;
+        // GameManager.SharedInstance.playerAgent.mover.commit = true;
+    }
+
+    private void DrawModeMouseDown()
+    {
+        if (_selectedFace && Ability == null)
+        {
+            _selectedFace._rend.material = _defaultMat;
+        }
+        else if (_selectedFace && Ability != null)
+        {
+            _selectedFace._rend.material = _abilityMat;
+        }
+        var ray = GameManager.SharedInstance.MainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(ray.origin, ray.direction);
+        float lowestHitDistance = 0;
+        Face lastFace = null;
+        for (int i = 0; i < hits.Length; i++)
+        {
+            m_HitInfo = hits[i];
+            var face = m_HitInfo.collider.gameObject.GetComponent<Face>();
+            if (!face)
+                continue;
+            float hitDistance = Vector3.Distance(m_HitInfo.point, face.gameObject.transform.parent.position);
+            if (lowestHitDistance == 0 || hitDistance < lowestHitDistance)
+                lowestHitDistance = hitDistance;
+            else if (hitDistance > lowestHitDistance)
+                continue;
+            if (lastFace)
+                lastFace._rend.material = _defaultMat;
+            face._rend.material = _selectedMat;
+            lastFace = face;
+            _selectedFace = face;
+
         }
     }
 
@@ -108,86 +148,11 @@ public class Face : MonoBehaviour
     {
         if (GameManager.SharedInstance.PlayMode)
         {
-            var ray = GameManager.SharedInstance.MainCamera.ScreenPointToRay(Input.mousePosition);
-            Vector3 point = Vector3.zero;
-            if (Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
-                GameManager.SharedInstance.playerAgent.agent.destination = m_HitInfo.point;
-            if (gameObject.CompareTag("Finish"))
-                GameManager.SharedInstance.playerAgent.goalPoint = m_HitInfo.point;
-            //point = m_HitInfo.point;
-
-            if (NavMesh.CalculatePath(GameManager.SharedInstance.playerAgent.transform.position,
-                m_HitInfo.point,
-                NavMesh.AllAreas,
-                path))
-                Debug.Log("PATH!");
-
-            if (path.corners.Length < 2)
-                return;
-
-            List<Vector3> moverPoints = new List<Vector3>();
-            for (int i = 1; i < path.corners.Length; i++)
-            {
-                Vector3 moverPoint = new Vector3(path.corners[i].x,
-                    GameManager.SharedInstance.playerAgent.transform.position.y,
-                    path.corners[i].z);
-                moverPoints.Add(moverPoint);
-            }
-
-            // GameManager.SharedInstance.playerAgent.mover.setPositions = moverPoints;
-            // GameManager.SharedInstance.playerAgent.mover.commit = true;
+            PlayModeMouseDown();
         }
         else
         {
-            if (_selectedFace && !_selectedFace.hasAbility)
-            {
-                _selectedFace._rend.material = _defaultMat;
-            }
-            else if (_selectedFace && _selectedFace.hasAbility)
-            {
-                _selectedFace._rend.material = _abilityMat;
-            }
-            var ray = GameManager.SharedInstance.MainCamera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit[] hits;
-            hits = Physics.RaycastAll(ray.origin, ray.direction);
-            float lowestHitDistance = 0;
-            Face lastFace = null;
-            for (int i = 0; i < hits.Length; i++)
-            {
-                m_HitInfo = hits[i];
-                var face = m_HitInfo.collider.gameObject.GetComponent<Face>();
-                if (!face)
-                    continue;
-                float hitDistance = Vector3.Distance(m_HitInfo.point, face.gameObject.transform.parent.position);
-                if (lowestHitDistance == 0 || hitDistance < lowestHitDistance)
-                    lowestHitDistance = hitDistance;
-                else if (hitDistance > lowestHitDistance)
-                    continue;
-                if (lastFace)
-                    lastFace._rend.material = _defaultMat;
-                face._rend.material = _selectedMat;
-                lastFace = face;
-                _selectedFace = face;
-
-            }
+            DrawModeMouseDown();
         }
-    }
-
-    private void OnMouseEnter()
-    {
-
-    }
-
-    private void OnMouseExit()
-    {
-
-    }
-
-    IEnumerator SetAgentPosition()
-    {
-        yield return new WaitForSeconds(0.01f);
-        var playerTransform = GameManager.SharedInstance.playerAgent.transform;
-        playerTransform.parent = null;
-        GameManager.SharedInstance.playerAgent.agent.enabled = true;
     }
 }
