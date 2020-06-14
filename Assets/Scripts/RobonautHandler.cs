fc8eba9b18384305be0f879f6e73aa6a1581da73
Remousamavi <remousamavi@gmail.com>
2020-06-14 18:25:09 -0400

    New character system with vector3 array pathfinding

--- Assets/Scripts/RobonautHandler.cs
+++ Assets/Scripts/RobonautHandler.cs
@@ -6,104 +6,121 @@ public class RobonautHandler : MonoBehaviour
 {
     [Header("Set In Inspector")]
     public Transform playerModel;
-    public bool setPositionFeed;
-    public float startSpin;
-    public float height;
-    public float spinRate;
+    public float playerSpeed;
+    public float spinSpeed;
     public float foreLean;
     public float sideLean;
 
     [Header("Dynamic Feed")]
-    public Vector3 targetPosition;
+    public Vector3[] targetPath;
+    public bool commit;
 
-    public Vector3 oldPosition;
-    public float oldRotation;
-    private float posX;
-    private float curX;
-    private float velX;
-    private float posZ;
-    private float curZ;
-    private float velZ;
-    private float eulerX;
-    public float eulerY;
-    private float eulerZ;
-    private float spinFactor;
-    public float velocity;
+    private Vector3 pos;
+    private Vector3 oldPos;
+    private int leg;
+    private bool moving;
+
+    private Vector3 dynamicPos;
+    private float dist;
+    private float vel;
+    private float multVel;
+    private float angle;
+    private float oldAngle;
     private float deltaAngle;
+    private float multAngle;
+
+    private float eulerX;
+    private float eulerY;
+    private float eulerZ;
 
     void Start()
     {
-        oldRotation = playerModel.eulerAngles.y;
-        posX = targetPosition.x;
-        curX = transform.position.x;
-        velX = 0;
-        posZ = targetPosition.z;
-        curZ = transform.position.z;
-        velZ = 0;
-        eulerX = 0;
-        eulerY = oldRotation;
-        eulerZ = 0;
-        spinFactor = 0.2f;
-        velocity = 0;
+        pos = transform.position;
+        oldPos = pos;
+        leg = 0;
+        moving = false;
+
+        dynamicPos = pos;
+        dist = 0;
+        vel = 0;
+        multVel = 0;
+        angle = 0;
+        oldAngle = 0;
         deltaAngle = 0;
+        multAngle = 0;
+
+        eulerX = 0;
+        eulerY = playerModel.eulerAngles.y;
+        eulerZ = 0;
     }
 
-    void Update()
+    private void Update()
     {
-        //Position
-        if (setPositionFeed)
+        if (commit)
         {
-            posX = targetPosition.x;
-            curX = transform.position.x;
-            posZ = targetPosition.z;
-            curZ = transform.position.z;
-            oldPosition = transform.position;
-            velocity = Mathf.Sqrt(Mathf.Pow(posZ - oldPosition.z, 2) + Mathf.Pow(posX - oldPosition.x, 2));
-            velX = Mathf.Lerp(curX, posX, velocity * Mathf.Clamp((-Mathf.Abs(deltaAngle) + 90) / 90, 0, 1) / 100);
-            velZ = Mathf.Lerp(curZ, posZ, velocity * Mathf.Clamp((-Mathf.Abs(deltaAngle) + 90) / 90, 0, 1) / 100);
-            transform.position = new Vector3(velX, height, velZ);
-            Rotator();
+            leg = 0;
+            moving = true;
+            commit = false;
         }
-        else
+        if (moving)
         {
-            posX = transform.position.x;
-            posZ = transform.position.z;
-            velocity = Mathf.Sqrt(Mathf.Pow(posZ - oldPosition.z, 2) + Mathf.Pow(posX - oldPosition.x, 2));
-            Rotator();
-            oldPosition = transform.position;
-        }
-    }
+            //Position
+            oldPos = pos;
+            dist = Mathf.Sqrt(Mathf.Pow(targetPath[leg].z - dynamicPos.z, 2) + Mathf.Pow(targetPath[leg].x - dynamicPos.x, 2));
+            angle = -Mathf.Atan2(targetPath[leg].z - pos.z, targetPath[leg].x - pos.x) * (180 / Mathf.PI) / 360 + 0.5f;
+            oldAngle = playerModel.eulerAngles.y;
+            dynamicPos += new Vector3(
+                (-Mathf.Cos(angle * 2 * Mathf.PI)) * (playerSpeed / 100),
+                0,
+                Mathf.Sin(angle * Mathf.PI * 2) * (playerSpeed / 100)
+            );
+            transform.position = new Vector3(
+                Mathf.LerpUnclamped(pos.x, dynamicPos.x, playerSpeed / 500 * multVel),
+                0,
+                Mathf.LerpUnclamped(pos.z, dynamicPos.z, playerSpeed / 500 * multVel)
+            );
+            pos = transform.position;
+            vel = Mathf.Sqrt(Mathf.Pow(oldPos.z - pos.z, 2) + Mathf.Pow(oldPos.x - pos.x, 2));
 
-    //Rotation
-    void Rotator()
-    {
-        oldRotation = playerModel.eulerAngles.y;
-        eulerY = (Mathf.Atan2(oldPosition.x - posX, oldPosition.z - posZ) * 180 / Mathf.PI) + startSpin;
-        deltaAngle = oldRotation - eulerY;
-        if (deltaAngle > 180)
-        {
-            eulerY += 360;
-            deltaAngle -= 360;
-        }
-        else if (deltaAngle < -180)
-        {
-            eulerY -= 360;
-            deltaAngle += 360;
-        }
-        if (setPositionFeed)
-        {
+            //Rotation
+            eulerY = (Mathf.Atan2(oldPos.x - pos.x, oldPos.z - pos.z) * 180 / Mathf.PI) + 180;
+            deltaAngle = oldAngle - eulerY;
+            multVel = Mathf.Clamp(24 / Mathf.Abs(deltaAngle), 0.1f, 1);
+            if (deltaAngle > 180)
+            {
+                eulerY += 360;
+                deltaAngle -= 360;
+            }
+            else if (deltaAngle < -180)
+            {
+                eulerY -= 360;
+                deltaAngle += 360;
+            }
             if (Mathf.Abs(deltaAngle) > 5)
             {
-                if (spinFactor < 1) { spinFactor += 0.01f; }
+                if (multAngle < 1) { multAngle += 0.01f; }
             }
             else
             {
-                if (spinFactor > 0.05f) spinFactor -= 0.01f;
+                if (multAngle > 0.05f) { multAngle -= 0.01f; }
+            }
+            eulerX = vel * foreLean;
+            eulerZ = sideLean * deltaAngle * vel * 0.1f;
+            playerModel.eulerAngles = new Vector3(eulerX, Mathf.Lerp(playerModel.eulerAngles.y, eulerY, spinSpeed / 100 * multAngle), eulerZ);
+            Debug.Log(multVel);
+
+            //Stop
+            if (leg < targetPath.Length - 1)
+            {
+                if (dist < 1) { leg++; }
+            }
+            else if (eulerX < 0.1f && eulerZ < 0.1f && vel < 0.1f)
+            {
+                moving = false;
+                playerModel.eulerAngles = new Vector3(0, playerModel.eulerAngles.y, 0);
+                transform.position = targetPath[targetPath.Length - 1];
+                leg = 0;
             }
         }
-        else { spinFactor = 1; }
-        eulerX = sideLean * deltaAngle * velocity * 0.1f;
-        eulerZ = velocity * foreLean;
-        playerModel.eulerAngles = new Vector3(eulerZ, Mathf.Lerp(oldRotation, eulerY, spinRate * spinFactor), eulerX);
     }
 }
