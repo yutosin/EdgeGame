eeec152dc23537561f9ffe71e05a28ef151c8122
nas <nmuhammad121@gmail.com>
2020-06-20 16:48:32 -0400

    -cleanup and integrate movement scripts

--- Assets/Scripts/DirectedAgent.cs
+++ Assets/Scripts/DirectedAgent.cs
@@ -3,49 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using Pathfinding;
 using UnityEngine;
-using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 public class DirectedAgent : MonoBehaviour
 {
     private RaycastHit _hitInfo;
     private Vector3 _goalPoint;
-    private bool _hasJump;
-    private Vector3 _jumpPoint;
     private Seeker _seeker;
     
-    public RobonautHandeler2 mover;
-    public Transform targetPosition;
+    public RobonautHandler mover;
 
     // Use this for initialization
     void Awake () 
     {
-        mover = GetComponent<RobonautHandeler2>();
+        mover = GetComponent<RobonautHandler>();
         _seeker = GetComponent<Seeker>();
         _hitInfo = new RaycastHit();
     }
 
-    private void Start()
-    {
-        // Start to calculate a new path to the targetPosition object, return the result to the OnPathComplete method.
-        // Path requests are asynchronous, so when the OnPathComplete method is called depends on how long it
-        // takes to calculate the path. Usually it is called the next frame.
-        //_seeker.StartPath(transform.position, targetPosition.position, OnPathComplete);
-    }
-
     public void OnPathComplete (Path p) {
         Debug.Log("Yay, we got a path back. Did it have an error? " + p.error);
-        Debug.Log(p.vectorPath[p.vectorPath.Count - 1]);
         mover.targetPath = p.vectorPath.ToArray();
         mover.commit = true;
     }
 
     private void Update()
     {
-        // if (Vector3.Distance(agent.nextPosition, _goalPoint) <= .6f)
-        // {
-        //     SceneManager.LoadScene("MainMenu");
-        // }
+        if (Vector3.Distance(transform.position, _goalPoint) <= .6f)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
     }
 
     public void HandleFacePointSelect()
@@ -53,13 +40,12 @@ public class DirectedAgent : MonoBehaviour
         var ray = GameManager.SharedInstance.MainCamera.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray.origin, ray.direction, out _hitInfo))
         {
-            //GameManager.SharedInstance.playerAgent.agent.destination = _hitInfo.point;
             var nearest = GameManager.SharedInstance.levelGraph.GetNearest(_hitInfo.point, NNConstraint.Default);
             var nearestNodePos = nearest.clampedPosition;
             _seeker.StartPath(transform.position, nearestNodePos, OnPathComplete);
         }
 
         if (_hitInfo.collider.gameObject.CompareTag("Finish"))
-            GameManager.SharedInstance.playerAgent._goalPoint = _hitInfo.point;
+            GameManager.SharedInstance.playerAgent._goalPoint = new Vector3(_hitInfo.point.x, _hitInfo.point.y + 0.5f, _hitInfo.point.z);
     }
 }
--- Assets/Scripts/RobonautHandler.cs
+++ Assets/Scripts/RobonautHandler.cs
@@ -4,88 +4,102 @@ using UnityEngine;
 
 public class RobonautHandler : MonoBehaviour
 {
-    [Header("Set In Inspector")]
+    //Script should be put in empty gameobject. Player model should be a child of the object and added to the playerModel transform below.
+    [Header("Set in Inspector")]
     public Transform playerModel;
     public float playerSpeed;
-    public float spinSpeed;
+    public float turnSpeed;
     public float foreLean;
     public float sideLean;
 
-    [Header("Dynamic Feed")]
+    [Header ("Dynamic")]
     public Vector3[] targetPath;
     public bool commit;
 
-    private Vector3 pos;
-    private Vector3 oldPos;
-    private int leg;
-    private bool moving;
-
-    private Vector3 dynamicPos;
+    private int stage;
+    private bool stageOn;
     private float dist;
+    private float baseDist;
     private float vel;
-    private float multVel;
     private float angle;
+    private float multAngle;
     private float oldAngle;
     private float deltaAngle;
-    private float multAngle;
-
     private float eulerX;
     private float eulerY;
     private float eulerZ;
+    private Vector3 pos;
+    private Vector3 oldPos;
+    private Vector3 dynPos;
+    private Vector3 basePos;
 
     void Start()
     {
-        pos = transform.position;
-        oldPos = pos;
-        leg = 0;
-        moving = false;
-
-        dynamicPos = pos;
+        commit = false;
+        stage = 0;
+        stageOn = false;
         dist = 0;
-        vel = 0;
-        multVel = 0;
+        baseDist = 0;
         angle = 0;
+        multAngle = 0;
         oldAngle = 0;
         deltaAngle = 0;
-        multAngle = 0;
-
         eulerX = 0;
-        eulerY = playerModel.eulerAngles.y;
+        eulerY = 0;
         eulerZ = 0;
+        
+        pos = transform.position;
+        oldPos = pos;
     }
 
     private void Update()
     {
         if (commit)
         {
-            leg = 0;
-            moving = true;
+            stage = 0;
+            basePos = transform.position;
+            stageOn = true;
             commit = false;
         }
-        if (moving)
+
+        if (stageOn)
         {
-            //Position
+            //Locomotion
             oldPos = pos;
-            dist = Mathf.Sqrt(Mathf.Pow(targetPath[leg].z - dynamicPos.z, 2) + Mathf.Pow(targetPath[leg].x - dynamicPos.x, 2));
-            angle = -Mathf.Atan2(targetPath[leg].z - pos.z, targetPath[leg].x - pos.x) * (180 / Mathf.PI) / 360 + 0.5f;
-            oldAngle = playerModel.eulerAngles.y;
-            dynamicPos += new Vector3(
-                (-Mathf.Cos(angle * 2 * Mathf.PI)) * (playerSpeed / 100),
+            int clampedStage = Mathf.Clamp(stage, 0, targetPath.Length - 1);
+            dist = Mathf.Sqrt(Mathf.Pow(targetPath[clampedStage].z - pos.z, 2) + Mathf.Pow(targetPath[clampedStage].x - pos.x, 2)); ;
+            baseDist = Mathf.Sqrt(Mathf.Pow(targetPath[clampedStage].z - basePos.z, 2) + Mathf.Pow(targetPath[clampedStage].x - basePos.x, 2));
+            angle = -Mathf.Atan2(targetPath[clampedStage].z - basePos.z, targetPath[clampedStage].x - basePos.x) * (180 / Mathf.PI) / 360 + 0.5f;
+            dynPos = new Vector3(
+                (-Mathf.Cos(angle * 2 * Mathf.PI)) * Mathf.Clamp(baseDist, 0, (playerSpeed / 100)),
                 0,
-                Mathf.Sin(angle * Mathf.PI * 2) * (playerSpeed / 100)
+                Mathf.Sin(angle * Mathf.PI * 2) * Mathf.Clamp(baseDist, 0, (playerSpeed / 100))
             );
-            transform.position = new Vector3(
-                Mathf.LerpUnclamped(pos.x, dynamicPos.x, playerSpeed / 500 * multVel),
-                0,
-                Mathf.LerpUnclamped(pos.z, dynamicPos.z, playerSpeed / 500 * multVel)
+            basePos += dynPos;
+            pos = new Vector3(
+                Mathf.LerpUnclamped(transform.position.x, basePos.x, playerSpeed / 200),
+                Mathf.Lerp(transform.position.y, targetPath[clampedStage].y, 0.05f),
+                Mathf.LerpUnclamped(transform.position.z, basePos.z, playerSpeed / 200)
             );
-            pos = transform.position;
+            transform.position = pos;
             vel = Mathf.Sqrt(Mathf.Pow(oldPos.z - pos.z, 2) + Mathf.Pow(oldPos.x - pos.x, 2));
+            if (baseDist < playerSpeed / 2000 && baseDist > -(playerSpeed / 2000))
+            {
+                stage++;
+                if (stage >= targetPath.Length)
+                {
+                    if (dist < playerSpeed / 8000 && dist > -(playerSpeed / 8000))
+                    {
+                        stageOn = false;
+                        transform.position = targetPath[targetPath.Length - 1];
+                    }
+                }
+            }
 
             //Rotation
             eulerY = (Mathf.Atan2(oldPos.x - pos.x, oldPos.z - pos.z) * 180 / Mathf.PI) + 180;
+            oldAngle = playerModel.eulerAngles.y;
             deltaAngle = oldAngle - eulerY;
-            multVel = Mathf.Clamp(24 / Mathf.Abs(deltaAngle), 0.1f, 1);
             if (deltaAngle > 180)
             {
                 eulerY += 360;
@@ -96,31 +110,10 @@ public class RobonautHandler : MonoBehaviour
                 eulerY -= 360;
                 deltaAngle += 360;
             }
-            if (Mathf.Abs(deltaAngle) > 5)
-            {
-                if (multAngle < 1) { multAngle += 0.01f; }
-            }
-            else
-            {
-                if (multAngle > 0.05f) { multAngle -= 0.01f; }
-            }
             eulerX = vel * foreLean;
             eulerZ = sideLean * deltaAngle * vel * 0.1f;
-            playerModel.eulerAngles = new Vector3(eulerX, Mathf.Lerp(playerModel.eulerAngles.y, eulerY, spinSpeed / 100 * multAngle), eulerZ);
-            Debug.Log(multVel);
-
-            //Stop
-            if (leg < targetPath.Length - 1)
-            {
-                if (dist < 1) { leg++; }
-            }
-            else if (eulerX < 0.1f && eulerZ < 0.1f && vel < 0.1f)
-            {
-                moving = false;
-                playerModel.eulerAngles = new Vector3(0, playerModel.eulerAngles.y, 0);
-                transform.position = targetPath[targetPath.Length - 1];
-                leg = 0;
-            }
+            playerModel.eulerAngles = new Vector3(0, Mathf.Lerp(playerModel.eulerAngles.y, eulerY, turnSpeed / 50), 0);
+            Debug.Log(deltaAngle);
         }
     }
 }
