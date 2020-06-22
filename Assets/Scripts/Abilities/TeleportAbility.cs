edc3735f19d926884d41c72a0bb32aa6d792afb3
alecphobia95 <46802114+alecphobia95@users.noreply.github.com>
2020-06-22 19:46:10 -0400

    Fixed TP

--- Assets/Scripts/Abilities/TeleportAbility.cs
+++ Assets/Scripts/Abilities/TeleportAbility.cs
@@ -15,7 +15,7 @@ public class TeleportAbility : MonoBehaviour, IFaceAbility
     public bool IsInitialized { get; set; }
     public int AbilityTimes { get; set; }
 
-    public Vector3 otherPos;
+    public Vector3 otherPos = new Vector3 (-1000, -1000, -1000);
     public Vector3 thisPos;
 
     public Vector3 FindCenter(Face face)
@@ -32,10 +32,15 @@ public class TeleportAbility : MonoBehaviour, IFaceAbility
 
     private void SetAgentPosition()//this has been copied from the elevator ability, probably can do this another way since parenting is not needed
     {
-        var playerTransform = GameManager.SharedInstance.playerAgent.transform;
-        Vector3 tpPos = otherPos;
-        tpPos.y = playerTransform.position.y + otherPos.y;
-        playerTransform.position = otherPos;
+        if (otherPos.x >= -999)
+        {
+            var playerTransform = GameManager.SharedInstance.playerAgent.transform;
+            Vector3 tpPos = otherPos;
+            tpPos.y = playerTransform.position.y + otherPos.y;
+            playerTransform.position = otherPos;
+            AbilityTimes--;
+        }
+
     }
 
     private void Start()
