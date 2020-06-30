5a4316bada69becabad77482baf5071c4a1ac9ba
alecphobia95 <46802114+alecphobia95@users.noreply.github.com>
2020-06-30 15:54:22 -0400

    Implemented Video System

---
+++ Assets/Scripts/PlayVideoScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class PlayVideoScript : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    private void Start()
+    {
+        videoPlayer.loopPointReached += CheckOver;
+    }
+
+    public IEnumerator PlayVideo()
+    {
+        videoPlayer.Prepare();
+        WaitForSeconds waitForSeconds = new WaitForSeconds(.5f);
+        while (!videoPlayer.isPrepared)
+        {
+            yield return waitForSeconds;
+            break;
+        }
+        GameManager.SharedInstance.uiManager.allUI.SetActive(false);
+        videoPlayer.Play();
+    }
+
+    private void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        videoPlayer.Stop();
+        GameManager.SharedInstance.uiManager.allUI.SetActive(true);
+    }
+
+}
