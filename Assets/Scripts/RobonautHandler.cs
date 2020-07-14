f34afcd595a52316e4084bcb8b552e91469d2cfb
Remousamavi <remousamavi@gmail.com>
2020-07-13 22:25:03 -0400

    - Finished fix for Robonaut

--- Assets/Scripts/RobonautHandler.cs
+++ Assets/Scripts/RobonautHandler.cs
@@ -13,19 +13,20 @@ public class RobonautHandler : MonoBehaviour
     public float turnSpeed;
     public float foreLean;
     public float sideLean;
+    public float climbSpeed;
     public float swingOut; //How far the robonaut will position beyond the side of a block when climbing.
 
     [Header("Dynamic")]
     public Vector3[] targetPath;
     public bool commit;
 
-    public int stage;
+    private int stage;
     private bool stageOn;
-    public int climbOn;
+    private int climbOn;
     private float dist;
-    private float baseDist;
     private float vel;
     private float angle;
+    private float targetAngle;
     private float oldAngle;
     private float deltaAngle;
     private float eulerY;
@@ -33,7 +34,7 @@ public class RobonautHandler : MonoBehaviour
     private Vector3 oldPos;
     private Vector3 dynPos;
     private Vector3 basePos;
-    public Vector3 goHere;
+    private Vector3 goHere;
 
     void Start()
     {
@@ -42,8 +43,8 @@ public class RobonautHandler : MonoBehaviour
         stageOn = false;
         climbOn = 0;
         dist = 0;
-        baseDist = 0;
         angle = 0;
+        targetAngle = 0;
         oldAngle = 0;
         deltaAngle = 0;
         eulerY = 0;
@@ -82,18 +83,17 @@ public class RobonautHandler : MonoBehaviour
             int clampedStage = Mathf.Clamp(stage, 0, targetPath.Length - 1);
             if (climbOn == 0) goHere = targetPath[clampedStage];
             dist = Mathf.Sqrt(Mathf.Pow(goHere.z - pos.z, 2) + Mathf.Pow(goHere.y - pos.y, 2) + Mathf.Pow(goHere.x - pos.x, 2));
-            baseDist = Mathf.Sqrt(Mathf.Pow(goHere.z - basePos.z, 2) + Mathf.Pow(goHere.y - basePos.y, 2) + Mathf.Pow(goHere.x - basePos.x, 2));
             angle = -Mathf.Atan2(goHere.z - basePos.z, goHere.x - basePos.x) * (180 / Mathf.PI) / 360 + 0.5f;
             dynPos = new Vector3(
-                (-Mathf.Cos(angle * 2 * Mathf.PI)) * Mathf.Clamp(baseDist, 0, (playerSpeed / 100)),
+                (-Mathf.Cos(angle * 2 * Mathf.PI)) * Mathf.Clamp(dist, 0, (playerSpeed / 100)),
                 0,
-                Mathf.Sin(angle * Mathf.PI * 2) * Mathf.Clamp(baseDist, 0, (playerSpeed / 100))
+                Mathf.Sin(angle * Mathf.PI * 2) * Mathf.Clamp(dist, 0, (playerSpeed / 100))
             );
             basePos += dynPos;
             if (climbOn == 2)
             {
-                if (pos.y > targetPath[stage].y) pos -= new Vector3(0, (playerSpeed / 1400), 0);
-                else if (pos.y < targetPath[stage].y) pos += new Vector3(0, (playerSpeed / 2000), 0);
+                if (pos.y > targetPath[stage].y) pos -= new Vector3(0, (climbSpeed / 100), 0);
+                else if (pos.y < targetPath[stage].y) pos += new Vector3(0, (climbSpeed / 100), 0);
             }
             else
             {
@@ -107,7 +107,7 @@ public class RobonautHandler : MonoBehaviour
             vel = Mathf.Sqrt(Mathf.Pow(oldPos.z - pos.z, 2) + Mathf.Pow(oldPos.x - pos.x, 2));
             if (climbOn == 0)
             {
-                if (baseDist < playerSpeed / 2000 && baseDist > -(playerSpeed / 2000))
+                if (dist < playerSpeed / 10 && dist > -(playerSpeed / 10))
                 {
                     stage++;
                     if (stage >= targetPath.Length)
@@ -144,15 +144,16 @@ public class RobonautHandler : MonoBehaviour
                             else
                             {
                                 //Go up-left (Negative Z).
-                                goHere = new Vector3(targetPath[stage - 1].x, targetPath[stage - 1].y, targetPath[stage - 1].z + 0.5f + ((targetPath[stage].y > pos.y) ? -swingOut : swingOut));
+                                goHere = new Vector3(targetPath[stage - 1].x, targetPath[stage - 1].y, targetPath[stage - 1].z - 0.5f - ((targetPath[stage].y > pos.y) ? -swingOut : swingOut));
                             }
                         }
+                        targetAngle = -Mathf.Atan2(targetPath[stage].z - basePos.z, targetPath[stage].x - basePos.x) * (180 / Mathf.PI) + 90;
                     }
                 }
             }
             else if (climbOn == 1)
             {
-                if (dist < 0.005f && dist > -0.005f)
+                if (dist < (climbSpeed / 20) && dist > -(climbSpeed / 20))
                 {
                     transform.position = goHere;
                     climbOn = 2;
@@ -171,7 +172,9 @@ public class RobonautHandler : MonoBehaviour
             }
 
             //Rotation
-            eulerY = (Mathf.Atan2(oldPos.x - pos.x, oldPos.z - pos.z) * 180 / Mathf.PI) + 180;
+            if (climbOn == 1) eulerY = targetAngle;
+            else if (climbOn == 2) eulerY = ((pos.y > targetPath[stage].y) ? targetAngle + 180 : targetAngle);
+            else eulerY = (Mathf.Atan2(oldPos.x - pos.x, oldPos.z - pos.z) * 180 / Mathf.PI) + 180;
             oldAngle = playerModel.eulerAngles.y;
             deltaAngle = oldAngle - eulerY;
             if (deltaAngle > 180)
